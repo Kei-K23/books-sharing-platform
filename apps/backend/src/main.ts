bb285e476ce225bfb31a7f9ef6b97e66b217c974
Kei-K23 <keiksl2301@gmail.com>
2025-04-12 14:49:21 +0630

    chore: fix vue-router client side routing from nestjs app

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,11 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'node:path';
+import { Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname, '../..', 'frontend/dist'));
 
+  const frontendDist = join(__dirname, '../..', 'frontend/dist');
+
+  // Serve static files
+  app.useStaticAssets(frontendDist);
+
+  // Serve Vue SPA fallback **only for non-API routes**
+  const expressApp = app.getHttpAdapter().getInstance();
+  expressApp.get(/^\/(?!api).*/, (req: Request, res: Response) => {
+    res.sendFile(join(frontendDist, 'index.html'));
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
