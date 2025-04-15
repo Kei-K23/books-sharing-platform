aefcf1551da8d279d6898ac27000682a640b4c1a
Kei-K23 <keiksl2301@gmail.com>
2025-04-15 15:48:02 +0630

    feat: add books detail page

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Index from "./pages/Index";
-import BookDetail from "./pages/BookDetail";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
@@ -10,6 +9,7 @@ import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
 import { Toaster } from "sonner";
 import Books from "./pages/Books";
+import BookDetail from "./components/books/BookDetail";
 
 const queryClient = new QueryClient();
 
