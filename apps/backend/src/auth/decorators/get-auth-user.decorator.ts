490d4a79d9593c51036d6b3bb45472fe2bc0236a
Kei-K23 <keiksl2301@gmail.com>
2025-04-16 11:28:46 +0630

    refactor the folder structure and remove frontend app

--- apps/backend/src/auth/decorators/get-auth-user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { UserEntity } from 'src/user/entities/user.entity';
-
-export const GetAuthUser = createParamDecorator(
-  (_data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest<{ user: UserEntity }>();
-    return request.user;
-  },
-);
--- apps/backend/src/auth/entities/auth.entity.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { UserEntity } from 'src/user/entities/user.entity';
-
-export class AuthEntity {
-  @ApiProperty()
-  accessToken: string;
-  @ApiProperty()
-  user: UserEntity;
-}
